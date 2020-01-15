4e0229333338cdd90703bded4a6307e04e5548db
Ernest Chechelski <ernichechelski@gmail.com>
2020-01-15 21:30:25 +0100

    Initial Commit

---
+++ Boards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boards
+//
+//  Created by Ernest Chechelski on 15/01/2020.
+//  Copyright © 2020 Ernest Chechelski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
